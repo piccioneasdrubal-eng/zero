df7f5662746b2fcfefd7974698811779bad0f9d4
piccioneasdrubal-eng <piccioneasdrubal@gmail.com>
2026-07-12 17:27:53 +0200

    Optimize Entity.js - O(1) swap-and-pop playerCellsList removal

--- core/Entity.js
+++ core/Entity.js
@@ -12,6 +12,7 @@ export default class Entity {
     isVirus;
     isFriend;
     agitated;
+    indexInList;
     constructor(id, accountID) {
         this.id = id;
         this.x = 0;
@@ -26,10 +27,23 @@ export default class Entity {
         this.agitated = false;
         this.isFriend = false;
         this.accountID = accountID;
+        this.indexInList = undefined;
     }
     destroy(bot) {
         delete bot.myCellIds[this.id];
         delete bot.playerCells[this.id];
+        
+        if (this.indexInList !== undefined && bot.playerCellsList) {
+            const list = bot.playerCellsList;
+            const lastCell = list[list.length - 1];
+            if (lastCell) {
+                list[this.indexInList] = lastCell;
+                lastCell.indexInList = this.indexInList;
+            }
+            list.pop();
+            this.indexInList = undefined;
+        }
+
         const index = bot.ownCells.indexOf(this);
         if (index !== -1)
             bot.ownCells.splice(index, 1);
