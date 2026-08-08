bd777f705e08bfad5f79d09a10d51ea10e290314
piccioneasdrubal-eng <piccioneasdrubal@gmail.com>
2026-08-08 21:14:34 +0200

    Add files via upload

---
+++ core/TurboMinion.js
@@ -0,0 +1,426 @@
+import WebSocket from "ws";
+import Entity from "./Entity.js";
+import { manager, updateLastBotAlive } from "../server.js";
+import { SmartBuffer } from "smart-buffer";
+import { buffers, helper, logger } from "../utils/index.js";
+import { config } from "../config/index.js";
+
+export class TurboMinion {
+  t;
+  ws;
+  rX;
+  rY;
+  client;
+  team;
+  isAlive;
+  proxyAgent;
+  offsetX;
+  offsetY;
+  borderX;
+  borderY;
+  isClosed;
+  ownCells;
+  gameModeInt;
+  playerCells;
+  followMouse;
+  isConnected;
+  isNearMouse;
+  encryptionKey;
+  facebookBots;
+  decryptionKey;
+  mapOffsetFixed;
+  moveInt;
+  errorTimeout;
+  myCellIds;
+  followMouseTimeout;
+  spawnTimeout;
+  spawnInterval;
+
+  constructor(client, team = 0) {
+    this.ws = null;
+    this.rX = 1;
+    this.rY = 1;
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.borderX = 0;
+    this.borderY = 0;
+    this.myCellIds = {};
+    this.ownCells = [];
+    this.moveInt = null;
+    this.t = manager.t();
+    this.client = client;
+    this.team = team;
+    this.isAlive = false;
+    this.isClosed = false;
+    this.gameModeInt = -1;
+    this.playerCells = [];
+    this.encryptionKey = 0;
+    this.decryptionKey = 0;
+    this.followMouse = false;
+    this.errorTimeout = null;
+    this.spawnTimeout = null;
+    this.spawnInterval = null;
+    this.isConnected = false;
+    this.isNearMouse = false;
+    this.facebookBots = false;
+    this.mapOffsetFixed = false;
+    this.followMouseTimeout = null;
+    this.proxyAgent = helper.getProxy();
+    this.connect();
+  }
+
+  connect() {
+    const serverUrl = this.client.getServer(this.team);
+    this.ws = new WebSocket(serverUrl, {
+      agent: this.proxyAgent,
+      headers: helper.generateHeaders(),
+      rejectUnauthorized: false,
+    });
+    this.ws.binaryType = "nodebuffer";
+    this.ws.onopen = this.onopen.bind(this);
+    this.ws.onclose = this.onclose.bind(this);
+    this.ws.onerror = this.onerror.bind(this);
+    this.ws.onmessage = this.onmessage.bind(this);
+  }
+
+  send(buffer, encrypt = false) {
+    if (!this.ws) return;
+    encrypt && (buffer = helper.xorBuffer(buffer, this.encryptionKey));
+    this.encryptionKey && (this.encryptionKey = helper.rotateKey(this.encryptionKey));
+    if (this.ws.readyState === 1) this.ws.send(buffer);
+  }
+
+  onopen() {
+    this.send(buffers.protocolVersion());
+    this.send(buffers.protocolKey());
+  }
+
+  onclose() {
+    this.isClosed = true;
+    if (this.team === 0) this.client.connectedBotsA--;
+    else this.client.connectedBotsB--;
+    if (this.t !== -1 && this.facebookBots) {
+      manager.releaseToken(this.t, () => {
+        this.t = -1;
+        this.facebookBots = false;
+      });
+    }
+  }
+
+  onerror() {
+    this.isClosed = true;
+    this.clearTimeouts();
+    this.clearIntervals();
+    this.facebookBots = false;
+    this.errorTimeout = setTimeout(() => {
+      if (this.ws?.readyState === WebSocket.CONNECTING || this.ws?.readyState === WebSocket.OPEN) {
+        this.ws.close();
+      }
+    }, 1000);
+  }
+
+  onmessage({ data: buffer }) {
+    let processedBuffer = buffer;
+    if (this.decryptionKey) {
+      processedBuffer = helper.xorBuffer(processedBuffer, this.decryptionKey ^ 31128);
+    }
+    this.handleBuffer(processedBuffer);
+  }
+
+  handleBuffer(buffer) {
+    const reader = SmartBuffer.fromBuffer(buffer);
+    const messageType = reader.readUInt8();
+    switch (messageType) {
+      case 18:
+        this.myCellIds = {};
+        this.ownCells = [];
+        this.playerCells = [];
+        break;
+      case 32:
+        const cellId = reader.readUInt32LE();
+        this.myCellIds[cellId] = cellId;
+        if (!this.isAlive) {
+          this.isAlive = true;
+          updateLastBotAlive();
+          this.moveInterval = setInterval(() => this.move(), 100);
+          if (!this.client.startedBots && !this.client.stoppedBots) {
+            this.client.startedBots = true;
+            logger.info("Bots started.");
+          }
+          if (!this.followMouseTimeout && !this.followMouse) {
+            this.followMouseTimeout = setTimeout(() => (this.followMouse = true), 7000);
+          }
+          if (this.t !== -1 && !this.facebookBots) {
+            manager.requestLogin(this.t, (loginBuffer) => {
+              this.send(loginBuffer, true);
+            });
+          }
+        }
+        break;
+      case 69:
+        this.ghostCells(reader);
+        break;
+      case 85:
+        logger.info("Mass boost activated");
+        break;
+      case 103:
+        this.facebookBots = true;
+        this.useMassBoost();
+        manager.buyMassBoost(this.t, (massBoostBuffer) => {
+          this.send(massBoostBuffer, true);
+        });
+        break;
+      case 104:
+        this.facebookBots = false;
+        manager.releaseToken(this.t, () => {
+          this.t = -1;
+          this.facebookBots = false;
+        });
+        break;
+      case 241:
+        this.isConnected = true;
+        if (this.team === 0) this.client.connectedBotsA++;
+        else this.client.connectedBotsB++;
+        const serverUrl = this.client.getServer(this.team);
+        if (!serverUrl) break;
+        this.decryptionKey = reader.readUInt32LE();
+        const serverMatch = serverUrl.match(
+          /wss:\/\/((web-arenas-live-[\w-]+\.agario\.miniclippt\.com\/[\w-]+\/[\d-]+))/
+        );
+        if (serverMatch && serverMatch[1]) {
+          this.encryptionKey = helper.murmur2("" + serverMatch[1] + reader.readStringNT("utf-8"), 255);
+        }
+        break;
+      case 242:
+        this.send(buffers.spawn(this.client.getBotName(this.team)), true);
+        break;
+      case 255:
+        this.handleMessage(
+          helper.uncompressBuffer(reader.toBuffer().subarray(5), Buffer.alloc(reader.readUInt32LE()))
+        );
+        break;
+    }
+  }
+
+  useMassBoost() {
+    const currentTime = Date.now();
+    const accountName = manager.ut[this.t]?.name;
+    if (!accountName) return;
+    const massBoostExpire = helper.getMassBoostExpire(accountName);
+    if (massBoostExpire && currentTime < massBoostExpire) return;
+    if (config.facebookBotSettings.useMassBoost && this.facebookBots) {
+      manager.buyMassBoost(this.t, (buyBuffer) => { this.send(buyBuffer, true); });
+      manager.setMassBoostExpire(this.t, (expireBuffer) => { this.send(expireBuffer, true); });
+      helper.clearExpiredMassBoosts();
+      helper.setMassBoostExpire(accountName, currentTime + 60 * 60 * 1000);
+      logger.info("Mass boost activated");
+    }
+  }
+
+  handleMessage(buffer) {
+    const reader = SmartBuffer.fromBuffer(buffer);
+    const messageType = reader.readUInt8();
+    switch (messageType) {
+      case 16: this.updateNodes(reader); break;
+      case 64: this.updateOffset(reader); break;
+    }
+  }
+
+  ghostCells(reader) {
+    reader.readOffset += 2;
+    const x = reader.readInt32LE() - this.offsetX;
+    const y = reader.readInt32LE() - this.offsetY;
+    let quadrant;
+    if (x < 0 && y < 0) quadrant = 1;
+    else if (x > 0 && y < 0) quadrant = 2;
+    else if (x > 0 && y > 0) quadrant = 3;
+    else quadrant = 4;
+    const quadrantMappings = [
+      [[1, 1], [-1, 1], [-1, -1], [1, -1]],
+      [[-1, 1], [1, 1], [1, -1], [-1, -1]],
+      [[-1, -1], [1, -1], [1, 1], [-1, 1]],
+      [[1, -1], [-1, -1], [-1, 1], [1, 1]],
+    ];
+    if (this.client.rQuadrant < 1 || this.client.rQuadrant > 4 || this.gameModeInt === 3) return;
+    const mapping = quadrantMappings[this.client.rQuadrant - 1];
+    [this.rX, this.rY] = mapping[quadrant - 1];
+  }
+
+  updateNodes(reader) {
+    const removedCount = reader.readUInt16LE();
+    for (let i = 0; i < removedCount; i++) {
+      if (this.playerCells[reader.readUInt32LE()]) {
+        const cell = this.playerCells[reader.readUInt32LE()];
+        if (cell) cell.destroy(this);
+      }
+    }
+    while (true) {
+      const cellId = reader.readUInt32LE();
+      if (cellId === 0) break;
+      const x = reader.readInt32LE();
+      const y = reader.readInt32LE();
+      const size = reader.readUInt16LE();
+      const flags = reader.readUInt8();
+      const isVirus = !!(flags & 1);
+      let skinName = null, color = null, name = null, extraFlags = 0;
+      if (flags & 128) extraFlags = reader.readUInt8();
+      if (flags & 2) color = helper.intToHex((reader.readUInt8() << 16) | (reader.readUInt8() << 8) | reader.readUInt8());
+      if (flags & 4) name = reader.readStringNT("utf8");
+      if (flags & 8) skinName = reader.readStringNT("utf8");
+      const isAgitated = !!(flags & 16);
+      const isFood = !!(extraFlags & 1);
+      const isFriend = !!(extraFlags & 2);
+      let accountId = 0;
+      if (extraFlags & 4) { reader.readOffset += 4; accountId = reader.readUInt32LE(reader.readOffset - 4); }
+      let cell = this.playerCells[cellId];
+      if (!cell) { cell = new Entity(cellId, accountId); this.playerCells[cellId] = cell; }
+      if (color !== null) cell.color = color;
+      if (skinName !== null) cell.name = unescape(encodeURIComponent(skinName));
+      if (name !== null) cell.skinName = name;
+      if (this.myCellIds[cellId] && this.ownCells.indexOf(cell) === -1) { cell.isMine = true; this.ownCells.push(cell); }
+      cell.x = x; cell.y = y; cell.size = size;
+      cell.isFood = isFood; cell.isVirus = isVirus;
+      cell.agitated = isAgitated; cell.isFriend = isFriend; cell.accountID = accountId;
+    }
+    const eatenCount = reader.readUInt16LE();
+    for (let i = 0; i < eatenCount; i++) {
+      const cellId = reader.readUInt32LE();
+      if (this.playerCells[cellId]) this.playerCells[cellId].destroy(this);
+    }
+    if (this.isAlive && this.ownCells.length === 0) {
+      if (this.moveInterval) { clearInterval(this.moveInterval); this.moveInterval = null; }
+      if (!this.facebookBots && this.followMouseTimeout) {
+        clearTimeout(this.followMouseTimeout);
+        this.followMouse = false;
+        this.followMouseTimeout = null;
+      }
+      this.isAlive = false;
+      this.isNearMouse = false;
+      const skinSettings = config.facebookBotSettings.skin;
+      if (skinSettings.enable && this.facebookBots) {
+        const randomIndex = Math.floor(Math.random() * skinSettings.names.length);
+        const skinName = skinSettings.names[randomIndex];
+        manager.changeSkin(this.t, skinName, (skinBuffer) => { this.send(skinBuffer, true); });
+      }
+      if (this.spawnInterval) clearInterval(this.spawnInterval);
+      this.spawnInterval = setInterval(() => {
+        if (this.isAlive) { clearInterval(this.spawnInterval); this.spawnInterval = null; return; }
+        this.send(buffers.spawn(this.client.getBotName(this.team)), true);
+      }, 50);
+    }
+  }
+
+  updateOffset(reader) {
+    const minX = reader.readDoubleLE(), minY = reader.readDoubleLE();
+    const maxX = reader.readDoubleLE(), maxY = reader.readDoubleLE();
+    if (!this.mapOffsetFixed) {
+      this.borderX = maxX - minX;
+      this.borderY = maxY - minY;
+      if (maxX - minX > 14000) this.offsetX = (minX + maxX) / 2;
+      if (maxY - minY > 14000) this.offsetY = (minY + maxY) / 2;
+      this.gameModeInt = reader.readUInt8();
+      this.mapOffsetFixed = true;
+    }
+  }
+
+  move() {
+    if (!this.isAlive) return;
+    const cells = this.ownCells;
+    if (cells.length === 0) return;
+
+    let cx = 0, cy = 0, csize = 0;
+    for (const { x, y, size } of cells) { cx += x; cy += y; csize += size; }
+    cx /= cells.length;
+    cy /= cells.length;
+
+    const clientX = this.client.userX / this.rX + this.offsetX;
+    const clientY = this.client.userY / this.rY + this.offsetY;
+    this.isNearMouse = Math.hypot(clientX - cx, clientY - cy) < 4000 + helper.size2mass(csize) * 0.5;
+
+    let targetX = clientX;
+    let targetY = clientY;
+
+    const useAI = (this.team === 0 ? this.client.botAiA : this.client.botAiB);
+    const useVShield = (this.team === 0 ? this.client.botVShieldA : this.client.botVShieldB);
+    const cMass = helper.size2mass(csize);
+    const massOver2000 = cMass > 2000;
+    const botName = this.client.getBotName(this.team);
+    const botNameEncoded = unescape(encodeURIComponent(botName));
+    const playerName = this.client.playerName;
+    const playerAlive = this.client.isAlive;
+
+    let nearestFood = null, minFoodDist = Infinity;
+    let nearestVirus = null, minVirusDist = Infinity;
+    const enemies = [];
+
+    for (const cell of Object.values(this.playerCells)) {
+      if (cell.isMine) continue;
+      if (cell.isFood && !cell.isFriend && !cell.isVirus && !cell.agitated) {
+        if (useAI || !this.followMouse) {
+          const d = helper.calculateDistance(cx, cy, cell.x, cell.y);
+          if (d < minFoodDist) { nearestFood = cell; minFoodDist = d; }
+        }
+        continue;
+      }
+      if (cell.isVirus && !cell.isFriend && !cell.isFood && !cell.agitated) {
+        if (useVShield) {
+          const d = helper.calculateDistance(cx, cy, cell.x, cell.y);
+          if (d < minVirusDist) { nearestVirus = cell; minVirusDist = d; }
+        }
+        continue;
+      }
+      if (cell.isFood || cell.isFriend || cell.isVirus) continue;
+      if (massOver2000) continue;
+      if (cell.name === playerName && playerAlive) continue;
+      if (cell.name === botNameEncoded) continue;
+      if (cell.size <= csize * 0.85) continue;
+      const dx = cell.x - cx, dy = cell.y - cy;
+      const distance = Math.hypot(dx, dy) - csize - cell.size;
+      if (distance < 150) enemies.push({ dx, dy, distance, sizeRatio: helper.size2mass(cell.size) / cMass });
+    }
+
+    if (enemies.length > 0) {
+      const dxM = clientX - cx, dyM = clientY - cy, md = 1 + Math.hypot(dxM, dyM);
+      let moveX = dxM / md, moveY = dyM / md;
+      for (const { dx, dy, distance, sizeRatio } of enemies) {
+        const force = -10 * sizeRatio;
+        moveX += ((dx / distance) * force) / distance;
+        moveY += ((dy / distance) * force) / distance;
+      }
+      const totalForce = 1 + Math.hypot(moveX, moveY);
+      targetX = cx + (moveX / totalForce) * 2000;
+      targetY = cy + (moveY / totalForce) * 2000;
+    } else if (this.followMouse) {
+      if (useAI && useVShield) {
+        if (nearestVirus) { targetX = nearestVirus.x; targetY = nearestVirus.y; }
+        else if (nearestFood) { targetX = nearestFood.x; targetY = nearestFood.y; }
+      } else if (!useAI && useVShield) {
+        if (nearestVirus && minVirusDist < 10000) { targetX = nearestVirus.x; targetY = nearestVirus.y; }
+      } else if (useAI && !useVShield) {
+        if (nearestFood) { targetX = nearestFood.x; targetY = nearestFood.y; }
+      }
+    } else {
+      if (nearestFood) { targetX = nearestFood.x; targetY = nearestFood.y; }
+    }
+
+    this.send(buffers.moveTo(targetX, targetY, this.decryptionKey), true);
+  }
+
+  clearIntervals() {
+    if (this.moveInterval) { clearInterval(this.moveInterval); this.moveInterval = null; }
+    if (this.spawnInterval) { clearInterval(this.spawnInterval); this.spawnInterval = null; }
+  }
+
+  clearTimeouts() {
+    if (this.spawnTimeout) { clearTimeout(this.spawnTimeout); this.spawnTimeout = null; }
+    if (this.errorTimeout) { clearTimeout(this.errorTimeout); this.errorTimeout = null; }
+    if (this.followMouseTimeout) { clearTimeout(this.followMouseTimeout); this.followMouseTimeout = null; }
+  }
+
+  stop() {
+    this.clearIntervals();
+    this.clearTimeouts();
+    this.ws?.terminate();
+    manager.clearTokenUsage();
+  }
+}
