47fc772b79b595331794c249df1a5b06b7585a83
piccioneasdrubal-eng <piccioneasdrubal@gmail.com>
2026-08-10 23:27:48 +0200

    Spawn interval: 50ms -> 100ms

--- core/Client.js
+++ core/Client.js
@@ -93,7 +93,7 @@ export default class Client {
         if (this.connectedBots < maxBots && this.bots.length < maxBots) {
           this.bots.push(new Minion(this));
         }
-      }, 50);
+      }, 100);
       this.countInt = setInterval(() => {
         this.bots = this.bots.filter((bot) => !bot.isClosed);
         const aliveBots = this.bots.filter(
